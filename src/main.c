aedcb3c38b4b455b483c44f9446843bcee79e1fb
RenanSpim <renan.sinhorini@unesp.br>
2024-06-08 13:04:39 -0300

    adicionado left e right vetices set, garantido que o grafo gerado será digrafo tambem

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "main.h"
 
 int getRandom(int ceil, int floor) {
@@ -15,24 +16,46 @@ void attributeRandomValuesToPoint(Point *p) {
     p->y = getRandom(10000, 0) / 100.0;
 }
 
-void attributeRandomNodesToVertex(Vertex *vert, Node *nodes, Node *fromNode) {
+int vertexHas(Vertex *vertArr, Vertex v, int currSize) {
+    for (int i = 0; i < currSize; i++) {
+        if ( // Compara os enderecos de nodes (n sei se funciona msm)
+            ((vertArr + i)->to == v.from && (vertArr + i)->from == v.to) ||
+            (vertArr + i)->from == v.to && (vertArr + i)->to == v.from
+        ) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void attributeRandomNodesToVertex(Vertex *vertArr, int i, Node *nodes, Node *fromNode) {
     Node *auxNode = (nodes + getRandom(NUM_NODES, 0));
 
     if (auxNode == fromNode) { // from e to, no Vertex, não podem ser iguais
-        return attributeRandomNodesToVertex(vert, nodes, fromNode);
+        return attributeRandomNodesToVertex(vertArr, i, nodes, fromNode);
+    }
+
+    if (vertexHas(vertArr, *(vertArr + i), i + 1)) { // Garante que o grafo vai ser digrafo
+        return;
     }
 
     if (fromNode == NULL) {
-        vert->from = auxNode;
+        (vertArr + i)->from = auxNode;
     } else {
-        vert->to = auxNode;
+        (vertArr + i)->to = auxNode;
     }
 }
 
 int main() {
-    Vertex *verts = malloc(sizeof(Vertex) * NUM_VERTEXES);
     Node *nodes = malloc(sizeof(Node) * NUM_NODES);
+
+    Vertex *verts = malloc(sizeof(Vertex) * NUM_VERTEXES);
+    Vertex *leftVertexSet = malloc(sizeof(Vertex) * ceil(NUM_VERTEXES / 2));
+    Vertex *rightVertexSet = malloc(sizeof(Vertex) * ceil(NUM_VERTEXES / 2));
+    
     Point *p;
+    
     int i;
 
     for (i = 0; i < NUM_NODES; i++) {
@@ -45,11 +68,13 @@ int main() {
     }
 
     for (i = 0; i < NUM_VERTEXES; i++) {
-        attributeRandomNodesToVertex((verts + i), nodes, NULL);
-        attributeRandomNodesToVertex((verts + i), nodes, (verts + i)->from);
+        attributeRandomNodesToVertex(verts, i, nodes, NULL);
+        attributeRandomNodesToVertex(verts, i, nodes, (verts + i)->from);
         
         printf("(%.2lf, %.2lf) -> (%.2lf, %.2lf)\n", (verts + i)->from->point.x, (verts + i)->from->point.y, (verts + i)->to->point.x, (verts + i)->to->point.y);
     }
+
+    
 
     return 0;
 }
